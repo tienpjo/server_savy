b7102d9d33951ade6fb2558da373a14d885e0e11
TienPJO <ductien.hust@gmail.com>
2019-11-12 16:03:52 +0700

    server_ready

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var gps_server = require('http').createServer(app);
+var io_socket = require('socket.io').listen(gps_server);
+users = [];
+srv_connect = [];
+
+
+gps_server.listen(process.env.PORT || 3000);
+console.log('Server running ... !!');
+
+app.get('/', function(req,res)
+{
+    res.sendFile(__dirname + '/index.html');
+});
+
+io_socket.sockets.on('connection', function (socket) {
+    socket.on("client-send",function (data) {
+        console.log("Server vua nhan duoc data la: " + JSON.stringify(data));
+        io_socket.sockets.emit("server-recv",data);
+    });
+});
