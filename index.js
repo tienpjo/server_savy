f72d3e4eec077637fc9524f63a2fd52ab8d43628
TienPJO <ductien.hust@gmail.com>
2019-11-26 17:15:45 +0700

    gf

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 var path = require("path");
 const app = express();
 var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+var io = require('socket.io').listen(server);
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -42,7 +42,6 @@ net.createServer(function(sock) {
     });
   });
 });
- 
 
  sock.on('close', function(data) {
    console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
