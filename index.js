755ef37ec520400be3e8893fd666eb7b9256f87d
TienPJO <ductien.hust@gmail.com>
2019-11-26 12:21:56 +0700

    up

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 var net = require('net');
+const express = require('express');
+const app = express();
+var path = require("path");
 
 var HOST = '103.137.185.94';
 var PORT = 9000;
 
-// Create a server instance, and chain the listen function to it
-// The function passed to net.createServer() becomes the event handler for the 'connection' event
-// The sock object the callback function receives UNIQUE for each connection
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/index.html'));
+});
+
+
 net.createServer(function(sock) {
-  // We have a connection - a socket object is assigned to the connection automatically
  console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
-  // Add a 'data' event handler to this instance of socket
   sock.on('data', function(data) {
     console.log('DATA :' + data);
-    // Write the data back to the socket, the client will receive it as data from the server
-  //  sock.write('You said "' + data + '"');
   });
-  // Add a 'close' event handler to this instance of socket
+ 
  sock.on('close', function(data) {
    console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
  });
