5a7a86d9d3a084df9d989b7571d2a8c1e9f28520
TienPJO <ductien.hust@gmail.com>
2019-11-27 10:51:22 +0700

    up

--- index.js
+++ index.js
@@ -6,26 +6,27 @@ var net = require('net');
 var HOST = '103.137.185.94';
 var PORT = 9000;
 var line;
+
 server.listen(3000);
 
 
-app.use(function(req, res, next) {
-        // Website you wish to allow to connect
-        res.setHeader('Access-Control-Allow-Origin', 'http://103.137.185.94:3000');
+// app.use(function(req, res, next) {
+//         // Website you wish to allow to connect
+//         res.setHeader('Access-Control-Allow-Origin', 'http://103.137.185.94:3000');
 
-        // Request methods you wish to allow
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//         // Request methods you wish to allow
+//         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     
-        // Request headers you wish to allow
-        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//         // Request headers you wish to allow
+//         res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     
-        // Set to true if you need the website to include cookies in the requests sent
-        // to the API (e.g. in case you use sessions)
-        res.setHeader('Access-Control-Allow-Credentials', true);
+//         // Set to true if you need the website to include cookies in the requests sent
+//         // to the API (e.g. in case you use sessions)
+//         res.setHeader('Access-Control-Allow-Credentials', true);
     
-        // Pass to next layer of middleware
-        next();
-  });
+//         // Pass to next layer of middleware
+//         next();
+//   });
 
 net.createServer(function(c) {
   console.log('client connected');
