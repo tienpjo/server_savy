722af0e922e7f5c65de923191c2e97cf2d49553f
TienPJO <ductien.hust@gmail.com>
2019-11-27 11:16:56 +0700

    upda

--- index.js
+++ index.js
@@ -4,6 +4,26 @@ var io = require('socket.io')(server);
 
 server.listen(3000);
 // WARNING: app.listen(80) will NOT work here!
+app.use(function (req, res, next) {
+  /*var err = new Error('Not Found');
+   err.status = 404;
+   next(err);*/
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
+
+//  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+  // Pass to next layer of middleware
+  next();
+});
+
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
