611877414628f3440620fa8be30585b3552bca50
TienPJO <ductien.hust@gmail.com>
2019-11-27 14:35:11 +0700

    up

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ app.options('*', cors());
 app.use(cors());
 server.listen(3000);
 
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
 
 
 app.use(function(req, res, next) {
@@ -31,28 +37,12 @@ app.use(function(req, res, next) {
         next();
   });
 
- io.on('connection', function (socket) {
-      socket.emit('news', { hello: 'world' });
-      // socket.on('my other event', function (data) {
-      //   console.log(data);
-      // });
-    });
-
-
 net.createServer(function(sock) {
   // We have a connection - a socket object is assigned to the connection automatically
  console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
   // Add a 'data' event handler to this instance of socket
   sock.on('data', function(data) {
     console.log('DATA ' + sock.remoteAddress + ': ' + data);
-    // Write the data back to the socket, the client will receive it as data from the server
-    // io.on('connection', function (socket) {
-    //   socket.emit('news', { hello: 'world' });
-    //   // socket.on('my other event', function (data) {
-    //   //   console.log(data);
-    //   // });
-    // });
-  //  sock.write('You said "' + data + '"');
   });
   // Add a 'close' event handler to this instance of socket
  sock.on('close', function(data) {
