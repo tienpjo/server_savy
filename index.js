31a996b78aa3963f4483e8ceb944a2506ce9cbc4
TienPJO <ductien.hust@gmail.com>
2019-11-27 09:57:01 +0700

    up

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-
-const express = require('express')
-, http = require('http');
-var path = require("path");
-const app = express();
-var server = http.createServer(app);
-var io = require('socket.io').listen(server);
+var express = require('express');
+var app = express();
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+var net = require('net');
+var HOST = '103.137.185.94';
+var PORT = 9000;
 
 app.use(function(req, res, next) {
         // Website you wish to allow to connect
@@ -24,44 +24,51 @@ app.use(function(req, res, next) {
         next();
   });
   
-  server.listen(3000);//, function () {
-//    // var addr = app.address();
-//     console.log('   app listening on http://' + addr.address + ':' + addr.port);
-//   });
+  var netServer = net.createServer(function(c) {
+    console.log('client connected');
+  
+    c.on('end', function() {
+      console.log('client disconnected');
+    });
+  
+    c.write('hello\r\n');
+    c.pipe(c);
+  });
+
+  netServer.listen(PORT, HOST);
+
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/views/index.html');
   });
 
-// io.sockets.on('connection', function(socket) {
-//     console.log('connection...');
-//         socket.on('emit_from_client', function(data) {
-//       console.log('socket.io server received : '+data);
-//       io.sockets.emit('emit_from_server', data);
-//     });
-//   });
 
-var net = require('net');
-var HOST = '103.137.185.94';
-var PORT = 9000;
-net.createServer(function(sock) {
- console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
- io.sockets.on('connection', function(socket) {
-    io.sockets.emit('emit_from_server', 'connected');
-    sock.on('data', function(data) {
-    var line = data.toString();
-        socket.on('emit_from_client', function(line) {
-      console.log('socket.io server received : '+line);
-      io.sockets.emit('emit_from_server', line);
-    });
+io.on('connection', function (socket) {
+ // socket.emit('news', { hello: 'world' });
+  socket.emit('news', function (data) {
+    console.log(data);
   });
 });
+server.listen(3000);
 
- sock.on('close', function(data) {
-   console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
+// net.createServer(function(sock) {
+//  console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
+//  io.sockets.on('connection', function(socket) {
+//     io.sockets.emit('emit_from_server', 'connected');
+//     sock.on('data', function(data) {
+//        var line = data.toString();
+//         socket.on('emit_from_client', function(line) {
+//       console.log('socket.io server received : '+line);
+//       io.sockets.emit('emit_from_server', line);
+//     });
+//   });
+// });
 
- });
+//  sock.on('close', function(data) {
+//    console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
+
+//  });
   
-}).listen(PORT, HOST);
+// }).listen(PORT, HOST);
 
 console.log('Server listening on ' + HOST +':'+ PORT);
