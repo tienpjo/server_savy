b3ecbd84e5b4ceb09c1f60e487487cdc74b42c00
TienPJO <ductien.hust@gmail.com>
2019-11-27 10:06:33 +0700

    update

--- index.js
+++ index.js
@@ -1,55 +1,69 @@
 var express = require('express');
-var app = express();
-var server = require("http").Server(app);
-var io = require("socket.io")(server);
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 var net = require('net');
 var HOST = '103.137.185.94';
 var PORT = 9000;
 
-app.use(function(req, res, next) {
-        // Website you wish to allow to connect
-        res.setHeader('Access-Control-Allow-Origin', 'http://103.137.185.94:3000');
-
-        // Request methods you wish to allow
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    
-        // Request headers you wish to allow
-        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    
-        // Set to true if you need the website to include cookies in the requests sent
-        // to the API (e.g. in case you use sessions)
-        res.setHeader('Access-Control-Allow-Credentials', true);
-    
-        // Pass to next layer of middleware
-        next();
-  });
-  
-  var netServer = net.createServer(function(c) {
-    console.log('client connected');
-  
-    c.on('end', function() {
-      console.log('client disconnected');
-    });
-  
-    c.write('hello\r\n');
-    c.pipe(c);
-  });
-
-  netServer.listen(PORT, HOST);
-
+server.listen(3000);
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/views/index.html');
-  });
+  res.sendFile(__dirname + '/index.html');
+});
 
 
 io.on('connection', function (socket) {
- // socket.emit('news', { hello: 'world' });
-  socket.emit('news', function (data) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
     console.log(data);
   });
 });
-server.listen(3000);
+
+// app.use(function(req, res, next) {
+//         // Website you wish to allow to connect
+//         res.setHeader('Access-Control-Allow-Origin', 'http://103.137.185.94:3000');
+
+//         // Request methods you wish to allow
+//         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    
+//         // Request headers you wish to allow
+//         res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    
+//         // Set to true if you need the website to include cookies in the requests sent
+//         // to the API (e.g. in case you use sessions)
+//         res.setHeader('Access-Control-Allow-Credentials', true);
+    
+//         // Pass to next layer of middleware
+//         next();
+//   });
+  
+  // var netServer = net.createServer(function(c) {
+  //   console.log('client connected');
+  
+  //   c.on('end', function() {
+  //     console.log('client disconnected');
+  //   });
+  
+  //   c.write('hello\r\n');
+  //   c.pipe(c);
+  // });
+
+  // netServer.listen(PORT, HOST);
+
+
+// app.get('/', function (req, res) {
+//     res.sendFile(__dirname + '/views/index.html');
+//   });
+
+
+// io.on('connection', function (socket) {
+//  // socket.emit('news', { hello: 'world' });
+//   socket.emit('news', function (data) {
+//     console.log(data);
+//   });
+// });
+// server.listen(3000);
 
 // net.createServer(function(sock) {
 //  console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
