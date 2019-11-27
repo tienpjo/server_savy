1211cc60b6d2e466e90e7415ba81e4f3e2f365f7
TienPJO <ductien.hust@gmail.com>
2019-11-27 11:06:57 +0700

    up

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(80);
+server.listen(3000);
 // WARNING: app.listen(80) will NOT work here!
 
 app.get('/', function (req, res) {
