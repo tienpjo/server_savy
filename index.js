d351b8ce7e5ee56aaa592462592e19f2ab20c69b
TienPJO <ductien.hust@gmail.com>
2019-11-27 14:57:53 +0700

    up

--- index.js
+++ index.js
@@ -21,20 +21,9 @@ io.on('connection', function (socket) {
 
 app.use(function(req, res, next) {
         // Website you wish to allow to connect
-        res.header('Access-Control-Allow-Origin', '*');
-
-        // Request methods you wish to allow
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    
-        // Request headers you wish to allow
-        res.header('Access-Control-Allow-Headers', 'Content-Type,X-Requested-With');
-    
-        // Set to true if you need the website to include cookies in the requests sent
-        // to the API (e.g. in case you use sessions)
-        res.setHeader('Access-Control-Allow-Credentials', true);
-    
-        // Pass to next layer of middleware
-        next();
+          res.header("Access-Control-Allow-Origin", "*");
+          res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+          next();
   });
 
 net.createServer(function(sock) {
