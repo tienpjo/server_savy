d5ea9658af408bb951eea0825ab620d3702ed513
TienPJO <ductien.hust@gmail.com>
2019-11-27 13:33:15 +0700

    up

--- index.js
+++ index.js
@@ -8,6 +8,23 @@ var PORT = 9000;
 
 server.listen(3000);
 
+app.use(function(req, res, next) {
+        // Website you wish to allow to connect
+        res.setHeader('Access-Control-Allow-Origin', 'http://103.137.185.94:3000');
+
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        res.setHeader('Access-Control-Allow-Credentials', true);
+    
+        // Pass to next layer of middleware
+        next();
+  });
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
@@ -25,12 +42,9 @@ net.createServer(function(sock) {
     sock.on('data', function(data) {
         var line = data.toString();
         console.log(line);
-        // socket.emit('news', line);
-        // socket.sock.emit('news', line);
-  //});
 });
  sock.on('close', function(data) {
-  console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
+console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
  });
   
 }).listen(PORT, HOST);
