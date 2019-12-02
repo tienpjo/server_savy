c646f1b32e3ad24f0f99c0edd8d8b148e6af359f
TienPJO <ductien.hust@gmail.com>
2019-12-02 10:12:36 +0700

    up

--- index.js
+++ index.js
@@ -30,7 +30,10 @@ net.createServer(function (sock) {
       // console.log('DATA ' + sock.remoteAddress + ': ' + data);
       var line = 'GPS_SAVY' + '---->' + new Date().toISOString() + '---->' + sock.remoteAddress.toString() + ' ---->' + data.toString();
       socket.emit('news', line);
-      console.log(data.toString());
+      var data_raw = data.toString();
+      var data_filter = data_raw.split(",").length;
+      console.log(data_filter[0]);
+      console.log(data_filter[2]);
       /*
       mongoClient.connect('mongodb://127.0.0.1:27017/db_server', function (err, db) {
         //neu ket noi khong thanh cong thi in ra loi
