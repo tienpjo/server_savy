4f02960a13d9f49bc589ae4cd5c34875a311fd98
TienPJO <ductien.hust@gmail.com>
2019-12-05 16:22:46 +0700

    up

--- routes/users.js
+++ routes/users.js
@@ -14,36 +14,65 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/:id', (req, res) => {
+  User.findById(req.params.id)
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      res.status(404).json({ success: false, msg: `No such user.` });
+    });
+});
+
 // UPDATE
-router.route('/edit/:id').get(function (req, res) {
-  let id = req.params.id;
-  User.findById(id, function (err, business){
-      res.json(business);
-  });
-});
+router.route('/edit/:id', (req, res) => {
+  let updatedUser = {
+    Lon: sanitizeName(req.body.Lon),
+    Lati: sanitizeEmail(req.body.Lati),
+    IP: sanitizeAge(req.body.IP),
+    // gender: sanitizeGender(req.body.gender)
+  };
 
-//  Defined update route
-router.route('/update/:id').post(function (req, res) {
-  User.findById(req.params.id, function(err, user) {
-      if (!user)
-          res.status(404).send("data is not found");
-      else {
-          console.log(user);
-          user.Lon = req.body.Lon;
-          user.Lati = req.body.Lati;
-          user.IP = req.body.IP;
-
-          user.save(function (error) {
-          if (err) throw err;
-          console.log('User Test successfully saved.');
+  User.findOneAndUpdate({ _id: req.params.id }, updatedUser, { runValidators: true, context: 'query' })
+    .then((oldResult) => {
+      User.findOne({ _id: req.params.id })
+        .then((newResult) => {
+          res.json({
+            success: true,
+            msg: `Successfully updated!`,
+            result: {
+              _id: newResult._id,
+              Lon: newResult.Lon,
+              Lati: newResult.Lati,
+              IP: newResult.IP
+              // gender: newResult.gender
+            }
+          });
         })
-              // .catch(err => {
-              //     res.status(400).send("unable to update the database");
-              // });
+        .catch((err) => {
+          res.status(500).json({ success: false, msg: `Something went wrong. ${err}` });
+          return;
+        });
+    })
+    .catch((err) => {
+      if (err.errors) {
+        if (err.errors.Lon) {
+          res.status(400).json({ success: false, msg: err.errors.Lon.message });
+          return;
+        }
+        if (err.errors.Lati) {
+          res.status(400).json({ success: false, msg: err.errors.Lati.message });
+          return;
+        }
+        if (err.errors.IP) {
+          res.status(400).json({ success: false, msg: err.errors.IP.message });
+          return;
+        }
+        // Show failed if all else fails for some reasons
+        res.status(500).json({ success: false, msg: `Something went wrong. ${err}` });
       }
-  });
+    });
 });
-
 
 router.route('/delete/:id').get(function (req, res) {
   User.findByIdAndRemove({_id: req.params.id}, function(err, person){
