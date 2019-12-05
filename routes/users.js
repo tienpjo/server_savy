e7ed902e83ebc886ab73fbd4838b19c361dabe47
TienPJO <ductien.hust@gmail.com>
2019-12-05 17:25:20 +0700

    update

--- routes/users.js
+++ routes/users.js
@@ -39,9 +39,9 @@ router.route('/update/:id').post(function (req, res) {
           res.status(404).send("data is not found");
       else {
           console.log(person);
-          person.Lon = req.body.Lon;
-          person.Lati = req.body.Lati;
-          person.ID_Device = req.body.ID_Device;
+          // person.Lon = req.body.Lon;
+          // person.Lati = req.body.Lati;
+          // person.ID_Device = req.body.ID_Device;
 
           person.save().then(business => {
               res.json('Update complete');
