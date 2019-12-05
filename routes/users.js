4628e3ad630eb4a0572374f9ddf4e57590a5ba39
TienPJO <ductien.hust@gmail.com>
2019-12-05 15:24:41 +0700

    up

--- routes/users.js
+++ routes/users.js
@@ -14,60 +14,33 @@ router.get('/', (req, res) => {
     });
 });
 
+// UPDATE
 router.route('/edit/:id').get(function (req, res) {
   let id = req.params.id;
-  User.findById(id, function (err, result){
-      res.json(result);
+  User.findById(id, function (err, business){
+      res.json(business);
   });
 });
 
+//  Defined update route
+router.route('/update/:id').post(function (req, res) {
+  User.findById(req.params.id, function(err, user) {
+      if (!user)
+          res.status(404).send("data is not found");
+      else {
+          console.log(user);
+          user.Lon = req.body.Lon;
+          user.Lati = req.body.Lati;
+          user.IP = req.body.IP;
 
-// UPDATE
-router.route('/update/:id', (req, res) => {
-
-  let updatedUser = {
-    Lon: sanitizeName(req.body.Lon),
-    Lati: sanitizeEmail(req.body.Lati),
-    ID_Device: sanitizeAge(req.body.ID_Device),
-  };
-
-  User.findOneAndUpdate({ _id: req.params.id }, updatedUser, { runValidators: true, context: 'query' })
-    .then((oldResult) => {
-      User.findOne({ _id: req.params.id })
-        .then((newResult) => {
-          res.json({
-            success: true,
-            msg: `Successfully updated!`,
-            result: {
-              _id: newResult._id,
-              Lon: newResult.Lon,
-              Lati: newResult.Lati,
-              ID_Device: newResult.ID_Device
-            }
-          });
-        })
-        .catch((err) => {
-          res.status(500).json({ success: false, msg: `Something went wrong. ${err}` });
-          return;
-        });
-    })
-    .catch((err) => {
-      if (err.errors) {
-        if (err.errors.Lon) {
-          res.status(400).json({ success: false, msg: err.errors.Lon.message });
-          return;
-        }
-        if (err.errors.Lati) {
-          res.status(400).json({ success: false, msg: err.errors.Lati.message });
-          return;
-        }
-        if (err.errors.IP) {
-          res.status(400).json({ success: false, msg: err.errors.IP.message });
-          return;
-        }
-        res.status(500).json({ success: false, msg: `Something went wrong. ${err}` });
+          person.save().then(business => {
+              res.json('Update complete');
+          })
+              .catch(err => {
+                  res.status(400).send("unable to update the database");
+              });
       }
-    });
+  });
 });
 
 
