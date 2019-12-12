05500d90e953a1a1334fa34b64183415bb39e124
TienPJO <ductien.hust@gmail.com>
2019-12-12 09:37:59 +0700

    up

--- routes/device.js
+++ routes/device.js
@@ -66,63 +66,63 @@ router.route('device/delete/:id').get(function (req, res) {
 
 // --------------------------USER - LOGIN----------------------------
 // get toan bo nguoi dung
-router.get('/', (req, res) => {
-  User_Login.find({}).lean()
-    .then((result) => {
-      res.json(result);
-    })
-    .catch((err) => {
-      res.status(500).json({ success: false, msg: `Something went wrong. ${err}` });
-    });
-});
+// router.get('/', (req, res) => {
+//   User_Login.find({}).lean()
+//     .then((result) => {
+//       res.json(result);
+//     })
+//     .catch((err) => {
+//       res.status(500).json({ success: false, msg: `Something went wrong. ${err}` });
+//     });
+// });
 
-// update nguoi dung 
+// // update nguoi dung 
 
-router.post('/user_edit', function (req, res, result) {
-  if (req.body.password !== req.body.passwordConf) {
-    var err = new Error('Passwords do not match.');
-    err.status = 400;
-    res.send("Passwords don't not match");
-    return result(err);
-  }
-  if (req.body.mobi &&
-    req.body.username &&
-    req.body.password &&
-    req.body.passwordConf &&
-    req.body.ID_Device) {
-    var userData = {
-      mobi = req.body.mobi,
-      username = req.body.username,
-      password = req.body.password,
-      ID_Device = req.body.ID_Device
-    }
-    User_Login.create(userData, function (error, user) {
-      if (error) {
-        return result(error);
-      } else {
-        // ID_Device = id;
-        return res.redirect('/edit/:ID_Device');
-      }
-    });
-  }
-  else {
-    var err = new Error('All fields required');
-    err.status = 400;
-    return result(err);
-  }
-})
+// router.post('/user_edit', function (req, res, result) {
+//   if (req.body.password !== req.body.passwordConf) {
+//     var err = new Error('Passwords do not match.');
+//     err.status = 400;
+//     res.send("Passwords don't not match");
+//     return result(err);
+//   }
+//   if (req.body.mobi &&
+//     req.body.username &&
+//     req.body.password &&
+//     req.body.passwordConf &&
+//     req.body.ID_Device) {
+//     var userData = {
+//       mobi = req.body.mobi,
+//       username = req.body.username,
+//       password = req.body.password,
+//       ID_Device = req.body.ID_Device
+//     }
+//     User_Login.create(userData, function (error, user) {
+//       if (error) {
+//         return result(error);
+//       } else {
+//         // ID_Device = id;
+//         return res.redirect('/edit/:ID_Device');
+//       }
+//     });
+//   }
+//   else {
+//     var err = new Error('All fields required');
+//     err.status = 400;
+//     return result(err);
+//   }
+// })
 
-router.get('user_logout', function (req, res, result) {
-  if (req.session) {
-    req.session.destroy(function (err) {
-      if (err) {
-        return result(err);
-      }
-      else {
-        return res.redirect('/');
-      }
-    })
-  }
-})
+// router.get('user_logout', function (req, res, result) {
+//   if (req.session) {
+//     req.session.destroy(function (err) {
+//       if (err) {
+//         return result(err);
+//       }
+//       else {
+//         return res.redirect('/');
+//       }
+//     })
+//   }
+// })
 
 module.exports = router;
