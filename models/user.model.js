75f5058a53930606bd06af7448eb61a9280c159e
TienPJO <ductien.hust@gmail.com>
2019-12-16 15:27:02 +0700

    up

--- models/user.model.js
+++ models/user.model.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const unique = require('mongoose-unique-validator');
 const UserSchema = new mongoose.Schema({
-    username: {type:String,unique:true,required:true},
+    username: {type:String,required:true},
     hash: {type:String,required:true},
     firstName:{type: String,required:true},
     lastName: {type:String,required:true},
-    mobile: {type:String,required:true},
+    mobile: {type:String,unique:true,required:true},
     address: {type:String,required:true},
 },
     {
