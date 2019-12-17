2079b2e112d167245fe6f49a6e3af78df62ba6c3
TienPJO <ductien.hust@gmail.com>
2019-12-17 14:02:33 +0700

    up

--- _service/user.service.js
+++ _service/user.service.js
@@ -46,9 +46,9 @@ async function authenticate({ mobile, password }) {
     }
     if (id_token)
     {
-      token = token;
-      Object.assign(id_token, token);
-      await id_token.save;
+      id_token.token = token;
+      // Object.assign(id_token, token);
+      await id_token.save();
     }
     return {
       // ...userWithoutHash,
