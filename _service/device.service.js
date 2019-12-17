34f184ba6aac97fb2618ff0a690fcfa9332e8966
TienPJO <ductien.hust@gmail.com>
2019-12-17 14:34:51 +0700

    up

--- _service/device.service.js
+++ _service/device.service.js
@@ -15,8 +15,8 @@ async function addDevice( deviceParam) {
     if (await Device.findOne({ id_device: deviceParam.id_device })) {
         throw 'Device "' + deviceParam.id_device + '" is already taken';
     }
-    const id = authMiddleware.getToken();
-    console.log(id);
+    // const id__1 = authMiddleware.getToken();
+    // console.log(id__1);
     const device = new Device(deviceParam);
     await device.save();
 }
