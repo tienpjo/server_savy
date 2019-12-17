0f06f011ef7e4ae7bc30b464371d0e0e375222b7
TienPJO <ductien.hust@gmail.com>
2019-12-17 17:29:36 +0700

    up

--- routes/user.js
+++ routes/user.js
@@ -1,69 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const userService = require('../_service/user.service');
-const deviceService = require('../_service/device.service');
+const apiUser = require("../controller/UserController");
+
 const AuthMiddleWare = require("../middleware/AuthMiddleware");
 const apiDevice = require("../routes/device");
 let initAPIs = (app) => {
-    router.post('/login', login);
-    router.post('/register', register);
+    router.post('/login', apiUser.login);
+    router.post('/register', apiUser.register);
     router.use(AuthMiddleWare.isAuth);
-    router.post('/add',add);
-    router.get('/', getAll);
-    router.get('/current', getCurrent);
-    router.get('/:id', getById);
-    router.get('/:id', update);
-    router.delete('/:id', _delete);
+    router.post('/add',apiUser.add);
+    router.get('/', apiUser.getAll);
+    router.get('/current', apiUser.getCurrent);
+    // router.get('/:id', getById);
+    // router.get('/:id', update);
+    // router.delete('/:id', _delete);
     return app.use("/users", router);
 }
 
 module.exports = initAPIs;
 
-function add (req, res, next) {
-    console.log(req.jwtDecoded.sub._id);
-    deviceService.addDevice(req.jwtDecoded.sub._id,req.body)
-        .then(() => res.json({}))
-        .catch(err => next(err));
-  };
-
-function login(req, res, next) {
-         userService.authenticate(req.body)
-        .then(user_mobi => user_mobi ? res.json(user_mobi) : res.status(400).json({ message: 'Username or password is incorrect' }))
-        .catch(err => next(err));
-}
-
-function register(req, res, next) {
-    userService.create(req.body)
-        .then(() => res.json({}))
-        .catch(err => next(err));
-}
-
-function getAll(req, res, next) {
-    userService.getAll()
-        .then(users => res.json(users))
-        .catch(err => next(err));
-}
-
-function getCurrent(req, res, next) {
-    userService.getById(req.user.sub)
-        .then(user => user ? res.json(user) : res.sendStatus(404))
-        .catch(err => next(err));
-}
-
-function getById(req, res, next) {
-    userService.getById(req.params.id)
-        .then(user => user ? res.json(user) : res.sendStatus(404))
-        .catch(err => next(err));
-}
-
-function update(req, res, next) {
-    userService.update(req.params.id, req.body)
-        .then(() => res.json({}))
-        .catch(err => next(err));
-}
-
-function _delete(req, res, next) {
-    userService.delete(req.params.id)
-        .then(() => res.json({}))
-        .catch(err => next(err));
-}
