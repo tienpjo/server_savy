37f8931ca7464635abd2c0d2ad73f7b86290d5e5
TienPJO <ductien.hust@gmail.com>
2019-12-17 10:42:40 +0700

    up

--- _service/device.service.js
+++ _service/device.service.js
@@ -2,6 +2,7 @@ const config = require('../config.json');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const dbs = require('../_helpers/database');
+const userService = require('../_service/user.service');
 const Device = dbs.Device;
 const User = dbs.User;
 module.exports = {
@@ -11,8 +12,13 @@ async function addDevice(deviceParam) {
     if (await Device.findOne({ id_device: deviceParam.id_device })) {
         throw 'Device "' + deviceParam.id_device + '" is already taken';
     }
-    const user = new User();
-    const device = new Device (deviceParam);
-    device.id_owner = user._id;
-    await device.save();
+    const tokenFromClient = req.body.token || req.query.token || req.headers["x-access-token"];
+    if (tokenFromClient) {
+        const decoded = await userService.verifyToken(tokenFromClient, config.secret);
+        // Nếu token hợp lệ, lưu thông tin giải mã được vào đối tượng req, dùng cho các xử lý ở phía sau.
+        // req.jwtDecoded = decoded;
+        device.id_owner = decoded.sub._id;
+        await device.save();
+    }
+
 }
