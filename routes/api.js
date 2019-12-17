3773c0daefc49c4358482a4ce9076849a676dd42
TienPJO <ductien.hust@gmail.com>
2019-12-17 16:21:36 +0700

    ip

--- routes/api.js
+++ routes/api.js
@@ -5,10 +5,10 @@ const apiUser = require("../controller/UserController");
 const AuthMiddleWare = require("../middleware/AuthMiddleware");
 
 let init_user_API = (app) => {
-    router.post('/register', apiUser.register);
-    router.post('/login', apiUser.login);
+    router.post("/register", apiUser.register);
+    router.post("/login", apiUser.login);
     router.use(AuthMiddleWare.isAuth);
-    router.post('/add',apiDevice.add);
+    router.post("/add",apiDevice.add);
     return app.use("/", router);
 }
 
