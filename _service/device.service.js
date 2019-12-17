e051c90dd5e48c2cc2fe76f8522e27a85f1b0cf3
TienPJO <ductien.hust@gmail.com>
2019-12-17 11:51:52 +0700

    up

--- _service/device.service.js
+++ _service/device.service.js
@@ -8,16 +8,10 @@ const User = dbs.User;
 module.exports = {
     addDevice
 }
-async function addDevice(req, res, deviceParam) {
+async function addDevice( deviceParam) {
     if (await Device.findOne({ id_device: deviceParam.id_device })) {
         throw 'Device "' + deviceParam.id_device + '" is already taken';
     }
-    const tokenFromClient = req.body.token || req.query.token || req.headers["x-access-token"];
-    const decoded = await userService.verifyToken(tokenFromClient, config.secret);
-    // Nếu token hợp lệ, lưu thông tin giải mã được vào đối tượng req, dùng cho các xử lý ở phía sau.
-    req.jwtDecoded = decoded;
-    console.log(decoded);
     const device = new Device(deviceParam);
     await device.save();
-
 }
