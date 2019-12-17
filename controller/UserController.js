5c0a6a2a96a10f4e787971a3592d9771daf6d5b5
TienPJO <ductien.hust@gmail.com>
2019-12-17 16:48:50 +0700

    up

--- controller/UserController.js
+++ controller/UserController.js
@@ -2,43 +2,43 @@ const express = require('express');
 const router = express.Router();
 const userService = require('../_service/user.service');
 
-function login(req, res, next) {
+async function login(req, res, next) {
      userService.authenticate(req.body)
     .then(user_mobi => user_mobi ? res.json(user_mobi) : res.status(400).json({ message: 'Username or password is incorrect' }))
     .catch(err => next(err));
 }
 
-function register(req, res, next) {
+async function register(req, res, next) {
     userService.create(req.body)
     .then(() => res.json({}))
     .catch(err => next(err));
 }
 
-function getAll(req, res, next) {
+async function getAll(req, res, next) {
 userService.getAll()
     .then(users => res.json(users))
     .catch(err => next(err));
 }
 
-function getCurrent(req, res, next) {
+async function getCurrent(req, res, next) {
 userService.getById(req.user.sub)
     .then(user => user ? res.json(user) : res.sendStatus(404))
     .catch(err => next(err));
 }
 
-function getById(req, res, next) {
+async function getById(req, res, next) {
 userService.getById(req.params.id)
     .then(user => user ? res.json(user) : res.sendStatus(404))
     .catch(err => next(err));
 }
 
-function update(req, res, next) {
+async function update(req, res, next) {
 userService.update(req.params.id, req.body)
     .then(() => res.json({}))
     .catch(err => next(err));
 }
 
-function _delete(req, res, next) {
+async function _delete(req, res, next) {
 userService.delete(req.params.id)
     .then(() => res.json({}))
     .catch(err => next(err));
