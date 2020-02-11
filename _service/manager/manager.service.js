f61801a0b8e80e18f1da8bc4be912009203589d2
TienPJO <ductien.hust@gmail.com>
2020-02-11 10:28:07 +0700

    up_test_func

--- _service/manager/manager.service.js
+++ _service/manager/manager.service.js
@@ -1,13 +1,7 @@
-const config = require('../../config.json');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
 const dbs = require('../../_helpers/database');
-const userService = require('../_service/user.service');
-const authMiddleware = require('../../middleware/AuthMiddleware')
 const Device = dbs.Device;
 const User = dbs.User;
-const Token = dbs.Token;
-const Tracking = dbs.Tracking;
+
 
 module.exports = {
     getOverView
