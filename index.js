c5c0ec95d237b5ffd3e7180ce2732683a29698c0
TienPJO <ductien.hust@gmail.com>
2020-02-14 15:10:36 +0700

    up

--- index.js
+++ index.js
@@ -44,9 +44,9 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-initAPIs(app);
 DeviceManagerApi(app);
 UserManagerApi(app);
+initAPIs(app);
 let mapSockets = [];
 
 app.post('/users/actionCtrl', function (req, res) {
