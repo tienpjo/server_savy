c084e923c465e71d096f2a1d05fb8d80f0c612ea
TienPJO <ductien.hust@gmail.com>
2020-02-14 15:30:32 +0700

    up

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.use(jwt());
 app.use(errHandler);
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
