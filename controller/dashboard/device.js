4d7a1fef95911ebe7b9490f4f4538d1541b8571e
TienPJO <ductien.hust@gmail.com>
2020-02-20 11:35:45 +0700

    up

--- controller/dashboard/device.js
+++ controller/dashboard/device.js
@@ -40,7 +40,7 @@ function deviceDelete(req, res, next) {
 function deviceGetDeviceList(req,res,next) {
     console.log(req.body);
     console.log('deviceId' + req.body.deviceId);
-    manageDevice.getDeviceListByDeviceId([req.body.deviceId])
+    manageDevice.getDeviceListByDeviceId(req.body)
         .then((device) => res.json({device}))
         .catch(err => next(err));
 }
