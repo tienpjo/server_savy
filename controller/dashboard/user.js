3c93cd00ac7192531fcdf6244eebf6d232a8706b
TienPJO <ductien.hust@gmail.com>
2020-02-22 11:51:10 +0700

    up

--- controller/dashboard/user.js
+++ controller/dashboard/user.js
@@ -66,12 +66,13 @@ function getById(req, res, next) {
 }
 
 function _delete(req, res, next) {
-    let id = req.params["_id"];
+    let id = req.params["id"];
+
     console.log(id);
     console.log(req.params._id);
-    managerUser._delete(req.params._id)
-        .then(() => res.json({}))
-        .catch(err => next(err));
+    // managerUser._delete(req.params._id)
+    //     .then(() => res.json({}))
+    //     .catch(err => next(err));
 }
 function addUser(req, res, next) {
     //  console.log(req.body.user);
