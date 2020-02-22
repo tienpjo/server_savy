13e59b7a7142a90ddc67eab8da4eae17a0ffcb0e
TienPJO <ductien.hust@gmail.com>
2020-02-22 14:25:31 +0700

    up

--- models/user.model.js
+++ models/user.model.js
@@ -1,16 +1,24 @@
 const mongoose = require('mongoose');
 const unique = require('mongoose-unique-validator');
 const UserSchema = new mongoose.Schema({
-    name:{type: String,required:true},                      // user cần khai báo tên
-    mobile: {type:String,unique:true,required:true},        // số đt
-    hash: {type:String,required:true},                      // mật khẩu sau khi hash
-    role:{type:String}
-    //address: {type:String,required:true},  
+    name: { type: String, required: true },                      // user cần khai báo tên
+    mobile: { type: String, unique: true, required: true },        // số đt
+    hash: { type: String, required: true },                      // mật khẩu sau khi hash
+    role: { type: String },
+    createdDate: { type: Number, default: Date.now }, // Thời gian tạo tài khoản
+    expiredDate: Number,                                // Thời gian hết hạn
+    storeInfo: {
+        name: String,
+        addr: String, // Địa chỉ
+        ward: String,
+        district: String,
+        city: String
+    }
 },
     {
         collection: 'user_dbs'
-    
-});
+
+    });
 UserSchema.plugin(unique, { message: 'That {PATH} is already taken.' });
-UserSchema.set('toJSON',{virtuals: true});
-const User = module.exports = mongoose.model('User',UserSchema);
+UserSchema.set('toJSON', { virtuals: true });
+const User = module.exports = mongoose.model('User', UserSchema);
