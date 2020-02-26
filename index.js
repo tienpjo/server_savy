fb05e9e1310ff71c42d738676c5be0d5a65d74d9
TienPJO <ductien.hust@gmail.com>
2020-02-26 15:14:45 +0700

    up

--- index.js
+++ index.js
@@ -63,11 +63,11 @@ server_tcp.on('connection', function (sock) {
   var data_filter;
   var id_device_gps;
   var line;
-   io.on('connection', function (socket) {
+  //  io.on('connection', function (socket) {
   sock.on('data', function (data) {
     line = 'GPS_SAVY' + '---->' + sock.remoteAddress.toString() + ' ---->' + data.toString();
      console.log(line);
-    socket.emit('news', line);
+    
     var data_raw = data.toString();
     data_filter = data_raw.split(',');
     if (data_filter[0] == "MOTO-ID") {
@@ -86,7 +86,7 @@ server_tcp.on('connection', function (sock) {
     }
     // sock.setTimeout(15000);
   });
-   });
+  //  });
   sock.on('timeout', () => {
   });
   sock.on('error', () => {
@@ -106,5 +106,5 @@ server_tcp.on('connection', function (sock) {
   });
 });
 io.on('connection', function (socket) {
-  
+  socket.emit('news', line);
 });
